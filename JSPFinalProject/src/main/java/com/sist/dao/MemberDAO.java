ea5e4b32b58214afa2378a07e59dd9c6eefecb93
seokyoung <quartz614@naver.com>
2022-03-02 23:36:27 +0900

    Study

--- JSPFinalProject/src/main/java/com/sist/dao/MemberDAO.java
+++ JSPFinalProject/src/main/java/com/sist/dao/MemberDAO.java
@@ -1,14 +1,17 @@
 package com.sist.dao;
 import java.util.*;
+
+import com.sist.vo.MemberVO;
+
 import java.sql.*;
 public class MemberDAO {
    private Connection conn;
    private PreparedStatement ps;
    private DBCPConnection dbcp=new DBCPConnection(); //연결/해제 => has-a
    //1.로그인 기능 
-   public String isLogin(String id,String pwd)
+   public MemberVO isLogin(String id,String pwd)
    {
-	   String result="";
+	   MemberVO vo=new MemberVO();
 	   try
 	   {
 		   conn=dbcp.getConnection();
@@ -23,7 +26,7 @@ public class MemberDAO {
 		   /////////////////////////////// ID존재 여부 확인 
 		   if(count==0) //ID가 없는 상태
 		   {
-			   result="NOID";
+			   vo.setMsg("NOID");;
 		   }
 		   else //ID가 있는 상태 
 		   {
@@ -35,16 +38,18 @@ public class MemberDAO {
 			   rs.next();
 			   String db_pwd=rs.getString(1);
 			   String name=rs.getString(2);
-			   int admin=rs.getInt(3);
+			   String admin=rs.getString(3); // y/n
 			   rs.close();
 			   
 			   if(db_pwd.equals(pwd)) // 로그인 
 			   {
-				   return name+"|"+admin;
+				   vo.setMsg("OK");
+				   vo.setName(name);
+				   vo.setAdmin(admin);
 			   }
 			   else //비밀번호가 틀리다 
 			   {
-				   result="NOPWD";
+				   vo.setMsg("NOPWD");
 			   }
 		   }
 		   
@@ -57,7 +62,67 @@ public class MemberDAO {
 	   {
 		   dbcp.disConnection(conn, ps);
 	   }
-	   return result;
+	   return vo;
+   }
+   // 아이디중복체크
+   public int memberIdcheck(String id)
+   {
+	   int count=0;
+	   try
+	   {
+		   conn=dbcp.getConnection();
+		   String sql="SELECT COUNT(*) FROM project_member "
+				     +"WHERE id=?";
+		   ps=conn.prepareStatement(sql);
+		   ps.setString(1, id);
+		   ResultSet rs=ps.executeQuery();
+		   rs.next();
+		   count=rs.getInt(1);
+		   rs.close();
+	   }catch(Exception ex)
+	   {
+		   ex.printStackTrace();
+	   }
+	   finally
+	   {
+		   dbcp.disConnection(conn, ps);
+	   }
+	   return count;
+   }
+   // 회원가입 
+   public void memberJoin(MemberVO vo)
+   {
+	   try
+	   {
+		   conn=dbcp.getConnection();
+		   String sql="INSERT INTO project_member VALUES(?,?,?,?,?,"
+				     +"?,?,?,?,?,?,'n')";
+		   ps=conn.prepareStatement(sql);
+		   ps.setString(1, vo.getId());
+		   ps.setString(2, vo.getPwd());
+		   ps.setString(3, vo.getName());
+		   ps.setString(4, vo.getSex());
+		   ps.setString(5, vo.getBirthday());
+		   ps.setString(6, vo.getEmail());
+		   ps.setString(7, vo.getPost());
+		   ps.setString(8, vo.getAddr1());
+		   ps.setString(9, vo.getAddr2());
+		   ps.setString(10, vo.getTel());
+		   ps.setString(11, vo.getContent());
+		   ps.executeUpdate();
+	   }catch(Exception ex)
+	   {
+		   ex.printStackTrace();
+	   }
+	   finally
+	   {
+		   dbcp.disConnection(conn, ps);
+	   }
    }
    
 }
+
+
+
+
+
